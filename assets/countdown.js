d7f43f38e4fd2723211e68041dd7904a09a8c18c
MysticalWizard <mmaring05@naver.com>
2022-05-04 14:24:07 +0900

    Update birthday

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var endDate = new Date("Jan 20, 2022 00:00:00").getTime();
+var endDate = new Date("Jan 20, 2023 00:00:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
     // Get todays date and time
@@ -12,13 +12,12 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     // Output the result in elements with id="cdHour/cdMin/cdSec"
-    document.getElementById("daysNum").innerHTML = ("0" + days).slice(-2);  
+    document.getElementById("daysNum").innerHTML = ("0" + days).slice(-3);  
     document.getElementById("hoursNum").innerHTML = ("0" + hours).slice(-2);
     document.getElementById("minsNum").innerHTML = ("0" + minutes).slice(-2);
     document.getElementById("secsNum").innerHTML = ("0" + seconds).slice(-2);
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countContainer").innerHTML = "EXPIRED";
     }
 }, 1000);
